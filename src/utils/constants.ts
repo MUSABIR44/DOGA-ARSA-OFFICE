5464ab437b94cb58fb47c85299f394f1fff85fc4
MUSABIR44 <2008musabir@gmail.com>
2025-05-04 17:36:40 +0300

    Numaralar güncellendi

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
-export const PHONE_NUMBER = '+90 532 455 66 23';
-export const WHATSAPP_URL = `https://wa.me/905324556623`;
+export const PHONE_NUMBER = '+90 542 287 96 44';
+export const WHATSAPP_URL = `https://wa.me/905422879644`;
 export const EMAIL = 'info@dogaarsaofisi.com';
 export const OFFICE_ADDRESS = 'Mimar Sinan, Alipaşa Cd. No:46/A, 34570 Silivri/İstanbul';
 export const GOOGLE_MAPS_URL = 'https://www.google.com/maps/dir//Mimar+Sinan,+Alipa%C5%9Fa+Cd.+no:46%2FA,+34570+Silivri%2F%C4%B0stanbul/@41.088472,28.1578583,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x14b53fce78a94c11:0xc5e5f3c41cbb0a60!2m2!1d28.2402591!2d41.0885014?entry=ttu&g_ep=EgoyMDI1MDQzMC4xIKXMDSoASAFQAw%3D%3D';
